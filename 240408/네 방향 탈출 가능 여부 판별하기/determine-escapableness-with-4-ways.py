c55386b0b2e60e9e147780a79e43d1720ce0e074
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-04-08 05:20:59 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 1889ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240408/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240408/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -3,16 +3,17 @@ from collections import deque
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-ans = 0
 
 q = deque()
 q.append((0, 0))
-while q:
-    x, y = q.popleft()
-    board[x][y] = 3
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
-            q.append((nx, ny))
+def bfs():
+    while q:
+        x, y = q.popleft()
+        board[x][y] = 3
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                q.append((nx, ny))
 
+bfs()
 print(1 if board[n-1][m-1] == 3 else 0)
