35b99e2ad135319db00ecc967dc6d0d0ce2027e9
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-09-12 20:22:34 +0900

    [Passed] 단순한 동전 챙기기 (98ms, 9MB)

--- trail4/단순한 동전 챙기기/collect-coins-easy.java
+++ trail4/단순한 동전 챙기기/collect-coins-easy.java
@@ -2,55 +2,60 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] pos = new int[10][2];
-    static int[] sPos, ePos;
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    static Point[] pos = new Point[10];
+    static Point sPos, ePos;
     static int ans = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < 10; i++) {
-            pos[i] = new int[] {-1, -1};
-        }
-
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < N; j++) {
                 char c = str.charAt(j);
-                if (c == 'S') {
-                    sPos = new int[] {i, j};
-                } else if (c == 'E') {
-                    ePos = new int[] {i, j};
-                } else if (c == '.') {
-                    continue;
-                } else {
-                    pos[c - '0'] = new int[] {i, j};
+                if (c == 'S') sPos = new Point(i, j);
+                else if (c == 'E') ePos = new Point(i, j);
+                else if (c != '.') {
+                    pos[c - '0'] = new Point(i, j);
                 }
             }
         }
 
-        choose(0, 0, 0, 0);
-        if (ans == Integer.MAX_VALUE) ans = -1;
-        System.out.println(ans);
+        choose(null, 1, 0, 0);
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
 
-    static void choose(int cur, int next, int cnt, int dist) {
+    static int getDist(Point a, Point b) {
+        return Math.abs(a.r - b.r) + Math.abs(a.c - b.c);
+    }
+
+    static void choose(Point cur, int next, int cnt, int dist) {
         if (cnt == 3) {
-            dist += (Math.abs(ePos[0] - pos[cur][0]) + Math.abs(ePos[1] - pos[cur][1]));
+            dist += getDist(cur, ePos);
             ans = Math.min(ans, dist);
             return;
         }
 
+        if (next > 9) return;
+
         for (int i = next; i < 10; i++) {
-            if (pos[i][0] == -1 && pos[i][1] == -1) continue;
+            if (pos[i] == null) continue;
             int t = 0;
             if (cnt == 0) {
-                t = (Math.abs(sPos[0] - pos[i][0]) + Math.abs(sPos[1] - pos[i][1]));
+                t = getDist(sPos, pos[i]);
             } else {
-                t = (Math.abs(pos[cur][0] - pos[i][0]) + Math.abs(pos[cur][1] - pos[i][1]));
+                t = getDist(cur, pos[i]);
             }
-            choose(i, i + 1, cnt + 1, dist + t);
+            choose(pos[i], i + 1, cnt + 1, dist + t);
         }
     }
 }
