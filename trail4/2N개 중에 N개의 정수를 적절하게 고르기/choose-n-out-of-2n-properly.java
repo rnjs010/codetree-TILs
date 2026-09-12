a94688ad189f2786409469c7887649efc7627e77
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-09-12 20:40:03 +0900

    [Passed] 2N개 중에 N개의 정수를 적절하게 고르기 (117ms, 9MB)

---
+++ trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, ans, total = 0;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            total += arr[i];
+        }
+        
+        ans = total;
+        comb(0, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void comb(int idx, int cnt, int sum) {
+        if (cnt == n) {
+            ans = Math.min(ans, Math.abs((total - sum) - sum));
+            return;
+        }
+
+        for (int i = idx; i < 2 * n; i++) {
+            comb(i + 1, cnt + 1, sum + arr[i]);
+        }
+    }   
+}
