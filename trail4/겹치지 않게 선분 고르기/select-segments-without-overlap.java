eb8ded84fc57084f3f40947d1c647ed8b817dfa2
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-09-17 12:50:18 +0900

    [Passed] 겹치지 않게 선분 고르기 (94ms, 9MB)

--- trail4/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ trail4/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -2,31 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static class Line implements Comparable<Line> {
+        int s, e;
+        public Line(int s, int e) {
+            this.s = s;
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(Line o) {
+            if (this.e == o.e) {
+                return Integer.compare(this.s, o.s);
+            }
+            return Integer.compare(this.e, o.e);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        int[][] lines = new int[n][2];
+        Line[] lines = new Line[n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            lines[i][0] = Integer.parseInt(st.nextToken());
-            lines[i][1] = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            lines[i] = new Line(s, e);
         }
 
-        Arrays.sort(lines, (a, b) -> {
-            if (a[1] == b[1]) {
-                return Integer.compare(a[0], b[0]);
-            }
-            return Integer.compare(a[1], b[1]);
-        });
+        Arrays.sort(lines);
 
         int ans = 0;
         int lastFinish = -1;
         for (int i = 0; i < n; i++) {
-            if (lastFinish < lines[i][0]) {
+            if (lastFinish < lines[i].s) {
                 ans++;
-                lastFinish = lines[i][1];
+                lastFinish = lines[i].e;
             }
         }
 
