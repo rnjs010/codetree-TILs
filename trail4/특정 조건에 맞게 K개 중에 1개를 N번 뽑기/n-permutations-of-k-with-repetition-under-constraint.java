9fb2687b4e02434a422198cee6b36ac0c103b944
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-09-20 14:49:00 +0900

    [Passed] 특정 조건에 맞게 K개 중에 1개를 N번 뽑기 (184ms, 19MB)

---
+++ trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int k, n;
+    static int[] output;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+
+        output = new int[n];
+        perm(0);
+        System.out.print(sb);
+    }
+
+    static void perm(int depth) {
+        if (depth == n) {
+            print_output();
+            return;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            if (depth < 2 || !(output[depth - 1] == output[depth - 2] && output[depth - 1] == i)) {
+                output[depth] = i;
+                perm(depth + 1);
+            }
+        }
+    }
+
+    static void print_output() {
+        for (int i = 0; i < n; i++) {
+            sb.append(output[i]).append(" ");
+        }
+        sb.append("\n");
+    }
+}
