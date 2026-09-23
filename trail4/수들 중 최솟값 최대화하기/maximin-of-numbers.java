daa369dcc634a35bd567da33a89d55ffb9617124
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-09-23 14:46:26 +0900

    [Passed] 수들 중 최솟값 최대화하기 (316ms, 9MB)

---
+++ trail4/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int n;
+    static int[][] grid;
+    static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        grid = new int[n][n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        perm(0, Integer.MAX_VALUE, 0);
+        System.out.print(ans);
+    }
+
+    static void perm(int row, int num, int visit) {
+        if (row == n) {
+            ans = Math.max(ans, num);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if ((visit & (1 << i)) != 0) continue;
+            perm(row + 1, Math.min(num, grid[row][i]), visit | (1 << i));
+        }
+    }
+}
